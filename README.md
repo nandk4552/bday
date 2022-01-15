f98489a11c3ebcb8e2b042032d8758eae97290dd
Nand Kishore <nandk4552@gmail.com>
2022-01-15 19:26:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Anima-Website
+Anima website with video Background With Beautiful animations
+# Live Preview On This Link : https://naughty-dijkstra-d293f9.netlify.app/
+## This is Responsive Website With background Video and Beautiful animation.
+## Feel Free to give any suggestion.
---
+++ js/script.js
@@ -0,0 +1,20 @@
+let dayNight = document.querySelector('.dayNight')
+let menuToggle = document.querySelector('.menuToggle')
+let body = document.querySelector('body')
+let navigation = document.querySelector('.navigation')
+
+dayNight.onclick = function() {
+    body.classList.toggle('dark')
+    dayNight.classList.toggle('active')
+}
+menuToggle.onclick = function() {
+        menuToggle.classList.toggle('active')
+        navigation.classList.toggle('active')
+    }
+    // typed script
+var typed = new Typed(".auto-input", {
+    strings: ["Happy", "Birthday", "Lavu"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
